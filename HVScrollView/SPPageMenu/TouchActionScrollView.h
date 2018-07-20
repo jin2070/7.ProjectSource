d9cd331cc3cfe4c31d7e72a60ee2bf9243cd0900
Kim chuncheng <chskim@163.com>
2018-07-21 07:16:57 +0800

    add menu TouchAction
    menu could be responser by touch, there scrollview and uiview class

---
+++ HVScrollView/SPPageMenu/TouchActionScrollView.h
@@ -0,0 +1,13 @@
+//
+//  TouchActionScrollView.h
+//  HVScrollView
+//
+//  Created by pc on 2018/7/20.
+//  Copyright © 2018年 iDress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TouchActionScrollView : UIScrollView
+@property(nonatomic)CGPoint FscrollMovepoint;
+@end
